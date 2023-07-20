cf6508ffbd3667303a17702ff7b28dbb7a9ce0e9
TheRealiPaul <86736500+TheRealiPaul@users.noreply.github.com>
2023-07-19 21:53:32 -0400

    Update create_view_score_length.sql

--- create_view_score_length.sql
+++ create_view_score_length.sql
@@ -2,5 +2,8 @@
 -- entre 0.0 et 1.0 dépendant de la longueur (comparé à la distribution) de la colonne louis_v004.crawl.html_content
 
 create or replace view score AS 
-SELECT (CAST(LENGTH(louis_v004.crawl.html_content) as float) / (SELECT SUM(LENGTH(louis_v004.crawl.html_content)) FROM louis_v004.crawl)) as score
-FROM louis_v004.crawl;
+SELECT (ABS(LENGTH(louis_v004.crawl.html_content) -
+(SELECT AVG(LENGTH(louis_v004.crawl.html_content))
+FROM louis_v004.crawl)) / (select stddev(length(louis_v004.crawl.html_content))
+FROM louis_v004.crawl)) as score
+from louis_v004.crawl;
